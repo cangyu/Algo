0ea7a5f86558ce887c851b86b740698d32893ee8
Yu Cang <yhcy1993@gmail.com>
2016-04-22 22:13:21 +0800

    start

---
+++ USACO/Section 2.2/subset/subset.cpp
@@ -0,0 +1,21 @@
+/*
+ID: yhcy1991
+PROG: subset
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	freopen("subset.in", "r", stdin);
+	freopen("subset.out", "w", stdout);
+
+
+
+	return 0;
+}
+
+
