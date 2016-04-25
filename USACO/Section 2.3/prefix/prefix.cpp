dda49a0a9b60f5a94b68ec4bca32cd3767032206
Yu Cang <yhcy1993@gmail.com>
2016-04-25 18:48:28 +0800

    bug fixed

--- USACO/Section 2.3/prefix/prefix.cpp
+++ USACO/Section 2.3/prefix/prefix.cpp
@@ -7,6 +7,7 @@ LANG: C++
 #include <fstream>
 #include <cstring>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
@@ -22,26 +23,36 @@ int f[200250];
 
 int pCnt, sLen;
 
-int solver();
+int solve();
 
 int main(int argc, char **argv)
 {
 	freopen("prefix.in", "r", stdin);
 	freopen("prefix.out", "w", stdout);
+	//freopen("prefix.err", "w", stderr);
 
-	cout << solver() << endl;
+	cout << solve() << endl;
 
 	return 0;
 }
 
-int solver()
+int solve()
 {
 	memset(f, 0, sizeof(f));
 	memset(s, 0, sizeof(s));
 	memset(p, 0, sizeof(p));
 	pCnt = sLen = 0;
 	while (cin >> p[pCnt].c && *(p[pCnt].c) != '.')
-		p[pCnt++].len = strlen(p[pCnt].c);
+	{
+		p[pCnt].len = strlen(p[pCnt].c);
+		pCnt++;//不能将这里的++写到上面的其中一个pCnt中，不同的编译器会采用不同的行为！！！
+	}
+
+	/*
+	cerr << "pCnt=" << pCnt << endl;
+	for (int i = 0; i < pCnt; i++)
+		cerr << "NO. " << i << " primitive is: " << p[i].c << " , and the length is: " << p[i].len << endl;
+	*/
 
 	char tmp[80];
 	while (cin >> tmp)
@@ -50,6 +61,11 @@ int solver()
 		memcpy(s + sLen, tmp, curLen);
 		sLen += curLen;
 	}
+
+	/*
+	cerr << "sLen=" << sLen << endl;
+	cerr << s << endl;
+	*/
 
 	for (int i = sLen - 1; i >= 0; i--)
 	{
