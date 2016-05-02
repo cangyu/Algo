36b18fd1e51a11c5b32b41fdb1ad80eeda277a97
Yu Cang <yhcy1993@gmail.com>
2016-05-02 19:09:25 +0800

    start

---
+++ USACO/Section 3.1/contact/contact.cpp
@@ -0,0 +1,22 @@
+/*
+ID: yhcy1991
+PROG: contact
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	freopen("contact.in", "r", stdin);
+	freopen("contact.out", "w", stdout);
+
+
+
+	return 0;
+}
