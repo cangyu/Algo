98429873cbaece28587ba0d4fa53cbaebf4fac15
Yu Cang <yhcy1993@gmail.com>
2016-05-03 09:50:08 +0800

    the final battle of section 3.1!

---
+++ USACO/Section 3.1/stamps/stamps.cpp
@@ -0,0 +1,21 @@
+/*
+ID: yhcy1991
+PROG: stamps
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	freopen("stamps.in", "r", stdin);
+	freopen("stamps.out", "w", stdout);
+
+
+	return 0;
+}
