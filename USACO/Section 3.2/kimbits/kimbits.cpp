57db44d31578898f77907e60a2db98db79a6b10f
Yu Cang <yhcy1993@gmail.com>
2016-05-04 15:42:32 +0800

    dp,compare the I and f[i][j] in the view like railway track,from bottom to up

--- USACO/Section 3.2/kimbits/kimbits.cpp
+++ USACO/Section 3.2/kimbits/kimbits.cpp
@@ -7,16 +7,15 @@ LANG: C++
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
 typedef unsigned int u32;
 
-u32 N, L, I;
+u32 N, L, I, f[32][32] = { 0 };
 
-inline int cntOnes(u32 t);
-void print(u32 x, int len);
+u32 calc(int n, int l);
 
 int main(int argc, char **argv)
 {
@@ -24,42 +23,31 @@ int main(int argc, char **argv)
 	freopen("kimbits.out", "w", stdout);
 
 	cin >> N >> L >> I;
+	memset(f, 0, sizeof(f));
 
-	u32 cnt = 0;
-	int w=-1;
-	while (cnt < I && w <= 2147483647)
+	for (int i = N; i >= 1; i--)
 	{
-		w++;
-		if (cntOnes(w) <= L)
-			cnt++;
+		calc(i - 1, L);
+		if (f[i - 1][L] < I)//n位数是由n-1位数在最前面补0或1得到的，f[i][j]是指所有长度为i的01串中，1的个数不超过j的01串的数量，递推方程类似组合恒等式，但是要注意边界
+		{
+			cout << 1;
+			I -= f[i - 1][L];
+			L--;
+		}
+		else
+			cout << 0;
 	}
-	
-	print(w, N);
+
+	cout << endl;
 	return 0;
 }
 
-inline int cntOnes(u32 t)
+u32 calc(int n, int l)
 {
-	u32 num = t;
-	num = (num & 0x55555555) + ((num & 0xAAAAAAAA) >> 1);
-	num = (num & 0x33333333) + ((num & 0xCCCCCCCC) >> 2);
-	num = (num & 0x0F0F0F0F) + ((num & 0xF0F0F0F0) >> 4);
-	num = (num & 0x00FF00FF) + ((num & 0xFF00FF00) >> 8);
-	num = (num & 0x0000FFFF) + ((num & 0xFFFF0000) >> 16);
-	return num;
-}
-
-void print(u32 x,int len)
-{
-	string ans;
-
-	while (x)
-	{
-		ans += (x % 2 + '0');
-		x /= 2;
-	}
-	ans.append(32 - ans.length(), '0');
-	reverse(ans.begin(), ans.end());
-	ans = ans.substr(32 - len, len);
-	cout << ans << endl;
+	if (n == 0 || l == 0)
+		return f[n][l] = 1;//注意设置边界
+	else if (f[n][l] != 0)
+		return f[n][l];
+	else
+		return	f[n][l] = calc(n - 1, l) + calc(n - 1, l - 1);//递推方程
 }
