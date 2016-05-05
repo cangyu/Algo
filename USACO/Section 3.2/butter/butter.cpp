898a81bd002561bb6e5092cd3f9537c827f3b783
Yu Cang <yhcy1993@gmail.com>
2016-05-05 09:13:58 +0800

    the last fighting

---
+++ USACO/Section 3.2/butter/butter.cpp
@@ -0,0 +1,23 @@
+/*
+ID: yhcy1991
+PROG: butter
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	freopen("butter.in", "r", stdin);
+	freopen("butter.out", "w", stdout);
+
+
+	return 0;
+}
