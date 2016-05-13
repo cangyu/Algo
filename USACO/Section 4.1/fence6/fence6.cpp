a8eb728577fd742c16225d184cd51f13eefb0d05
Yu Cang <yhcy1993@gmail.com>
2016-05-13 21:33:43 +0800

    todo

---
+++ USACO/Section 4.1/fence6/fence6.cpp
@@ -0,0 +1,20 @@
+/*
+ID: yhcy1991
+PROG: fence6
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char **argv) 
+{
+	freopen("fence6.in", "r", stdin);
+	freopen("fence6.out", "w", stdout);
+
+
+	return 0;
+}
