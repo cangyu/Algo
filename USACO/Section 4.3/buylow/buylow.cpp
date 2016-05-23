db228cf1df91594499292fc93416c8971f4d81a5
Yu Cang <yhcy1993@gmail.com>
2016-05-23 11:24:11 +0800

    todo repetition check

--- USACO/Section 4.3/buylow/buylow.cpp
+++ USACO/Section 4.3/buylow/buylow.cpp
@@ -7,6 +7,12 @@ LANG: C++
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <map>
+
+int n;
+int a[5004] = { 0 };
+int f[5004] = { 0 };
+int c[5004] = { 0 };
 
 using namespace std;
 
@@ -15,7 +21,32 @@ int main(int argc, char **argv)
 	freopen("buylow.in", "r", stdin);
 	freopen("buylow.out", "w", stdout);
 
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", a + i);
 
+	fill_n(f, sizeof(f) / sizeof(int), 1);
+
+	for (int i = n - 1; i >= 1; i--)
+	{
+		for (int k = i + 1; k <= n; k++)
+		{
+			if (a[i] > a[k])
+				f[i] = max(f[i], 1 + f[k]);
+		}
+	}
+
+	int maxIndex = 0, fmax = -1;
+	for (int i = 1; i <= n; i++)
+	{
+		if (f[i] > fmax)
+		{
+			fmax = f[i];
+			maxIndex = i;
+		}
+	}
+
+	cout << fmax << ' ';
 
 	return 0;
 }
